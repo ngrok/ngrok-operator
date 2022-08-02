78efad63941f1390b28cf71bcf25275ac74de521
Alex Bezek <alex@ngrok.com>
2022-08-02 13:43:03 -0700

    refactor controller receiver from old "t" for tunnels to "ir" for ingress reconciler

--- internal/controllers/ingress_controller.go
+++ internal/controllers/ingress_controller.go
@@ -34,11 +34,11 @@ type IngressReconciler struct {
 // It is invoked whenever there is an event that occurs for a resource
 // being watched (in our case, ingress objects). If you tail the controller
 // logs and delete, update, edit ingress objects, you see the events come in.
-func (t *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := t.Log.WithValues("ingress", req.NamespacedName)
+func (ir *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := ir.Log.WithValues("ingress", req.NamespacedName)
 	// TODO: Figure out the best way to form the edgeName taking into account isolating multiple clusters
 	edgeName := strings.Replace(req.NamespacedName.String(), "/", "-", -1)
-	edgeNamespace, err := t.LogicalEdgeNamespace(ctx)
+	edgeNamespace, err := ir.LogicalEdgeNamespace(ctx)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -48,14 +48,14 @@ func (t *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	// I believe this client provided by the controller-runtime uses a cache
 	// It might be better to do in the ManagerSetup with a filter later on
 	ingressClasses := &netv1.IngressClassList{}
-	if err := t.List(ctx, ingressClasses); err != nil {
+	if err := ir.List(ctx, ingressClasses); err != nil {
 		log.Error(err, "unable to list ingress classes")
 		return ctrl.Result{}, err
 	}
 	log.Info(fmt.Sprintf("found %s ingress classes", ingressClasses))
 
 	ingress := &netv1.Ingress{}
-	if err := t.Get(ctx, req.NamespacedName, ingress); err != nil {
+	if err := ir.Get(ctx, req.NamespacedName, ingress); err != nil {
 		if client.IgnoreNotFound(err) == nil {
 			log.Info("ingress not found, must have been deleted")
 			return ctrl.Result{}, nil
