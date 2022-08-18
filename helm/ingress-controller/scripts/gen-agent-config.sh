d272d75799974f23aa3b4f28100698e127076a55
alex bezek <alex.liam.bezek@gmail.com>
2022-08-17 18:27:33 -0700

    Correct ngrok to ngrok

--- helm/ingress-controller/scripts/gen-agent-config.sh
+++ helm/ingress-controller/scripts/gen-agent-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NGORK_LOG="${NGROK_LOG:-stdout}"
+NGROK_LOG="${NGROK_LOG:-stdout}"
 NGROK_METADATA="${NGROK_METADATA:-{}}"
 NGROK_REGION="${NGROK_REGION:-us}"
 NGROK_REMOTE_MANAGEMENT="${NGROK_REMOTE_MANAGEMENT:-true}"
@@ -9,7 +9,7 @@ cat > /var/lib/ngrok/agent-template.yaml <<EOF
 version: 2
 authtoken: $NGROK_AUTHTOKEN
 console_ui: false
-log: $NGORK_LOG
+log: $NGROK_LOG
 metadata: $NGROK_METADATA
 region: $NGROK_REGION
 remote_management: $NGROK_REMOTE_MANAGEMENT
