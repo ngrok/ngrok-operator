a3239114d7a0a602587dae76616e632171b946ae
Alex Bezek <alex@ngrok.com>
2022-12-21 16:11:32 -0800

    remove the server addr

--- helm/ingress-controller/scripts/gen-agent-config.sh
+++ helm/ingress-controller/scripts/gen-agent-config.sh
@@ -6,7 +6,6 @@ cat > /var/lib/ngrok/agent-template.yaml <<EOF
 version: 2
 authtoken: $NGROK_AUTHTOKEN
 console_ui: false
-server_addr: $NGROK_SERVER_ADDR
 region: $NGROK_REGION
 remote_management: $NGROK_REMOTE_MANAGEMENT
 update_check: false
