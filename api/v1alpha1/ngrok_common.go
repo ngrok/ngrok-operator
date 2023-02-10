8805d982b6cb0a4cc1751e4e7364f0d5944fd4f2
Alex Bezek <alex@ngrok.com>
2023-02-09 22:01:14 -0800

    Update various other missed spots

--- api/v1alpha1/ngrok_common.go
+++ api/v1alpha1/ngrok_common.go
@@ -3,10 +3,10 @@ package v1alpha1
 // common ngrok API/Dashboard fields
 type ngrokAPICommon struct {
 	// Description is a human-readable description of the object in the ngrok API/Dashboard
-	// +kubebuilder:default:=`Created by ngrok-ingress-controller`
+	// +kubebuilder:default:=`Created by kubernetes-ingress-controller`
 	Description string `json:"description,omitempty"`
 	// Metadata is a string of arbitrary data associated with the object in the ngrok API/Dashboard
-	// +kubebuilder:default:=`{"owned-by":"ngrok-ingress-controller"}`
+	// +kubebuilder:default:=`{"owned-by":"kubernetes-ingress-controller"}`
 	Metadata string `json:"metadata,omitempty"`
 }
 
