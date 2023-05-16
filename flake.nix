13553adc8055357e540f6d9431afcfdf984696e1
Josh Robson Chase <josh@robsonchase.com>
2023-05-16 11:54:40 -0400

    misc: add direnv and a nix devshell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go_1_20
+          gotools
+          go-tools
+          golangci-lint
+        ];
+      };
+    }));
+}
