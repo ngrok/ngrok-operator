0f613906c625fec26bc7583b4f63d9d1d79a637e
Celina Ward <celina@ngrok.com>
2023-07-05 22:20:41 -0700

    update documentation, add preflight check success output

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -7,4 +7,6 @@ GOVERSION="$(go env GOVERSION || echo "not installed")"
 if ! [[ "$GOVERSION" == "go1.20" ||  "$GOVERSION" = "go1.20."* ]]; then
   echo "Detected go version $GOVERSION, but 1.20 is required"
   exit 1
+else
+  echo "Preflight passed ✅"
 fi
