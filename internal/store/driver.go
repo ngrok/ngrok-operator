01db7f08e6507c21fddb1c71847ab9ea330d9150
Abdirahman Osman <abdirahman.osmanthus@gmail.com>
2024-03-27 09:58:03 -0500

    search for service in httproute namespace

--- internal/store/driver.go
+++ internal/store/driver.go
@@ -947,7 +947,7 @@ func (d *Driver) calculateHTTPSEdgesFromGateway(edgeMap map[string]ingressv1alph
 
 								refName := string(backendref.Name)
 								//refNamespace := string(*backendref.Namespace)
-								serviceUID, servicePort, err := d.getEdgeBackendRef(backendref.BackendRef, gtw.Namespace)
+								serviceUID, servicePort, err := d.getEdgeBackendRef(backendref.BackendRef, httproute.Namespace)
 								if err != nil {
 									d.log.Error(err, "could not find port for service", "namespace", gtw.Namespace, "service", refName)
 								}
