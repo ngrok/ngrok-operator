d6a4f71cfe9c74ef166dfe6fdfee2b953b89e5a6
Jonathan Stacks <jonstacks@users.noreply.github.com>
2024-08-30 04:26:15 -0500

    chore(deps): Update go 1.22 -> 1.23 (#417)
    As part of the upgrade, also upgrade golangci-lint and other
    dependencies.

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -4,8 +4,8 @@ set -eu -o pipefail
 
 GOVERSION="$(go env GOVERSION || echo "not installed")"
 
-if ! [[ "$GOVERSION" == "go1.22" ||  "$GOVERSION" = "go1.22."* ]]; then
-  echo "Detected go version $GOVERSION, but 1.22 is required"
+if ! [[ "$GOVERSION" == "go1.23" ||  "$GOVERSION" = "go1.23."* ]]; then
+  echo "Detected go version $GOVERSION, but 1.23 is required"
   exit 1
 else
   echo "Preflight passed ✅"
