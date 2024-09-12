03678400e5077b0ea861836c823c995902004352
Harrison Katz <hkatz@ngrok.com>
2024-09-12 16:34:25 -0400

    Add util functions for GroupVersionKind and printing client.Object

--- internal/util/k8s.go
+++ internal/util/k8s.go
@@ -15,3 +15,73 @@ func ToClientObjects[T any, PT interface {
 	}
 	return objs
 }
+
+// ObjectsToName converts a client.Object to its name
+func ObjToName(obj client.Object) string {
+	if obj == nil {
+		return ""
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	if gvk.Empty() {
+		return ""
+	}
+
+	return obj.GetName()
+}
+
+// ObjToKind converts a client.Object to its kind
+func ObjToKind(obj client.Object) string {
+	if obj == nil {
+		return ""
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	if gvk.Empty() {
+		return ""
+	}
+
+	return gvk.Kind
+}
+
+// ObjToGroupVersionKind converts a client.Object to its GVK
+func ObjToGVK(obj client.Object) string {
+	if obj == nil {
+		return ""
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	if gvk.Empty() {
+		return ""
+	}
+
+	return gvk.String()
+}
+
+// ObjToHumanName converts a client.Object to a human-readable name
+func ObjToHumanName(obj client.Object) string {
+	if obj == nil {
+		return ""
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	if gvk.Empty() {
+		return ""
+	}
+
+	return gvk.Kind + "/" + obj.GetName()
+}
+
+// ObjToHumanGvkName converts a client.Object to a human-readable full name including GroupVersionKind
+func ObjToHumanGvkName(obj client.Object) string {
+	if obj == nil {
+		return ""
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	if gvk.Empty() {
+		return ""
+	}
+
+	return gvk.String() + " Name=" + obj.GetName()
+}
