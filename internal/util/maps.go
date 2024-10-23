666f993f1e89854e28bbe3605c0b9336439e204c
Jonathan Stacks <jonstacks@users.noreply.github.com>
2024-10-23 04:14:04 -0500

    feat: add util to merge multiple maps

---
+++ internal/util/maps.go
@@ -0,0 +1,12 @@
+package util
+
+// MergeMaps merges multiple maps into a single map giving precedence to the last map in the list
+func MergeMaps[K comparable, V any](maps ...map[K]V) map[K]V {
+	result := make(map[K]V)
+	for _, m := range maps {
+		for k, v := range m {
+			result[k] = v
+		}
+	}
+	return result
+}
