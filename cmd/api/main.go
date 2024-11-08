6e2c6e7d672c35942b370d5c215d7b2d756769d5
Jonathan Stacks <jonstacks@users.noreply.github.com>
2024-11-08 11:08:30 -0600

    fix: Better migration path from the ngrok kuberntes-ingress-controller to the ngrok-operator (#495)
    * fix: Better migration path from the ngrok kuberntes-ingress-controller to the ngrok-operator
    
    * chore: rename k8sClinet -> k8sClient

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -217,12 +217,12 @@ func runController(ctx context.Context, opts managerOpts) error {
 
 	// create default config and clientset for use outside the mgr.Start() blocking loop
 	k8sConfig := ctrl.GetConfigOrDie()
-	k8sClinet, err := client.New(k8sConfig, client.Options{Scheme: scheme})
+	k8sClient, err := client.New(k8sConfig, client.Options{Scheme: scheme})
 	if err != nil {
 		return fmt.Errorf("unable to create k8s client: %w", err)
 	}
 
-	if err := createKubernetesOperator(ctx, k8sClinet, opts); err != nil {
+	if err := createKubernetesOperator(ctx, k8sClient, opts); err != nil {
 		return fmt.Errorf("unable to create KubernetesOperator: %w", err)
 	}
 
@@ -236,11 +236,18 @@ func runController(ctx context.Context, opts managerOpts) error {
 	var k8sResourceDriver *store.Driver
 	if opts.enableFeatureIngress || opts.enableFeatureGateway {
 		// we only need a driver if these features are enabled
-		if driver, err := getK8sResourceDriver(ctx, mgr, opts); err != nil {
+		k8sResourceDriver, err = getK8sResourceDriver(ctx, mgr, opts)
+		if err != nil {
 			return fmt.Errorf("unable to create Driver: %w", err)
-		} else {
-			k8sResourceDriver = driver
 		}
+
+		// Run a migration for migrating from the old ingress controller to the operator
+		// TODO: Delete me after the initial releae of the ngrok-operator
+		setupLog.Info("Migrating Kubernetes Ingress Controller labels to ngrok operator")
+		if err := k8sResourceDriver.MigrateKubernetesIngressControllerLabelsToNgrokOperator(ctx, k8sClient); err != nil {
+			return fmt.Errorf("unable to migrate Kubernetes Ingress Controller labels to ngrok operator: %w", err)
+		}
+		setupLog.Info("Kubernetes Ingress controller labels migrated to ngrok operator")
 	}
 
 	if opts.enableFeatureIngress {
