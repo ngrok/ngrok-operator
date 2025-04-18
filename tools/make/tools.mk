7102a5880acca4412099abd24c5d9f9bd12115d4
Alice Lilith <a.wasko@ngrok.com>
2025-04-18 11:17:32 -0700

    split up makefile (#629)
    * split up makefile
    
    Signed-off-by: Alice-Lilith <a.wasko@ngrok.com>
    
    * add tools/make to docker build ctx
    
    Signed-off-by: Alice-Lilith <a.wasko@ngrok.com>
    
    ---------
    
    Signed-off-by: Alice-Lilith <a.wasko@ngrok.com>

---
+++ tools/make/tools.mk
@@ -0,0 +1,49 @@
+##@ Tools/Dependencies
+
+# go-install-tool will 'go install' any package with custom target and name of binary, if it doesn't exist
+# $1 - target path with name of binary (ideally with version)
+# $2 - package url which can be installed
+# $3 - specific version of package
+define go-install-tool
+@[ -f $(1) ] || { \
+set -e; \
+package=$(2)@$(3) ;\
+echo "Downloading $${package}" ;\
+GOBIN=$(LOCALBIN) go install $${package} ;\
+mv "$$(echo "$(1)" | sed "s/-$(3)$$//")" $(1) ;\
+}
+endef
+
+
+.PHONY: preflight
+preflight: ## Verifies required things like the go version
+	scripts/preflight.sh
+
+
+.PHONY: kustomize
+kustomize: $(KUSTOMIZE) ## Download kustomize locally if necessary.
+$(KUSTOMIZE): $(LOCALBIN)
+	$(call go-install-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v5,$(KUSTOMIZE_VERSION))
+
+
+.PHONY: controller-gen
+controller-gen: $(CONTROLLER_GEN) ## Download controller-gen locally if necessary.
+$(CONTROLLER_GEN): $(LOCALBIN)
+	$(call go-install-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen,$(CONTROLLER_TOOLS_VERSION))
+
+
+.PHONY: envtest
+envtest: $(ENVTEST) ## Download setup-envtest locally if necessary.
+$(ENVTEST): $(LOCALBIN)
+	$(call go-install-tool,$(ENVTEST),sigs.k8s.io/controller-runtime/tools/setup-envtest,$(ENVTEST_VERSION))
+
+
+.PHONY: golangci-lint
+golangci-lint: $(GOLANGCI_LINT) ## Download golangci-lint locally if necessary.
+$(GOLANGCI_LINT): $(LOCALBIN)
+	$(call go-install-tool,$(GOLANGCI_LINT),github.com/golangci/golangci-lint/cmd/golangci-lint,${GOLANGCI_LINT_VERSION})
+
+
+.PHONY: _helm_setup
+_helm_setup: ## Setup helm dependencies
+	$(MAKE) -C $(HELM_CHART_DIR) setup
