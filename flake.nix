16e62a3db38cddd818775fb6b7a7bc20ea0caa24
Jonathan Stacks <jonstacks@users.noreply.github.com>
2025-10-16 13:30:26 -0400

    feat: Codespaces based development environment (#685)
    * refactor: Sort and nixfmt the list of nix packages
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * feat: Add kind and tilt to devenv
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * feat: Add initial dev container
    
    ---------
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,24 +4,39 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go_1_24
-          gotools
-          go-tools
-          golangci-lint
-          kubernetes-helm
-          kubebuilder
-          jq
-          yq
-          kyverno-chainsaw
-        ];
-        CGO_ENABLED = "0";
-      };
-    }));
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_24
+            go-tools
+            golangci-lint
+            gotools
+            jq
+            kind
+            kubebuilder
+            kubernetes-helm
+            kyverno-chainsaw
+            nixfmt-rfc-style
+            tilt
+            yq
+          ];
+
+          CGO_ENABLED = "0";
+          # GitHub Codespaces sets GOROOT in /etc/environment. However, we are managing
+          # go via nix, so we need to unset it to avoid conflicts. See also: https://dave.cheney.net/2013/06/14/you-dont-need-to-set-goroot-really
+          GOROOT = "";
+        };
+      }
+    ));
 }
