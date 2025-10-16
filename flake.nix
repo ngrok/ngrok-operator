44748ce7c4b9901ea1ad8844d2475fcd07aa5bdd
Jonathan Stacks <jonstacks@users.noreply.github.com>
2025-10-16 09:39:03 -0500

    refactor: Sort and nixfmt the list of nix packages
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,24 +4,37 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: (flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go_1_24
-          gotools
-          go-tools
-          golangci-lint
-          kubernetes-helm
-          kubebuilder
-          jq
-          yq
-          kyverno-chainsaw
-        ];
-        CGO_ENABLED = "0";
-      };
-    }));
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_24
+            go-tools
+            golangci-lint
+            gotools
+            jq
+            kubebuilder
+            kubernetes-helm
+            kyverno-chainsaw
+            nixfmt-rfc-style
+            yq
+          ];
+
+          CGO_ENABLED = "0";
+          # GitHub Codespaces sets GOROOT in /etc/environment. However, we are managing
+          # go via nix, so we need to unset it to avoid conflicts. See also: https://dave.cheney.net/2013/06/14/you-dont-need-to-set-goroot-really
+          GOROOT = "";
+        };
+      }
+    ));
 }
