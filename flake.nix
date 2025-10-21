1fc35be6a65f4ff2e3d4f51187fdf51ce4871851
Jonathan Stacks <jonstacks@users.noreply.github.com>
2025-10-21 00:13:17 -0500

    Codespaces pt 4 (#689)
    * feat: Add ngrok to flake so we can run ngrok binary in the devenv
    
    This allow for testing things like bindings within the development environment
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * feat: Add recommended extensions for devenv
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * feat: Add hostRequirements for devcontainer
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * feat: Add devhelp message to the nix flake
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>
    
    * docs: Add documentation for the Codespaces developer environment
    
    ---------
    
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs =
     {
       self,
@@ -13,7 +12,47 @@
     (flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfreePredicate = pkg: pkg.pname == "ngrok";
+          };
+        };
+
+        mkScript = name: text: let
+          script = pkgs.writeShellScriptBin name text;
+        in script;
+
+        scripts = [
+          (mkScript "devhelp" ''
+cat <<'EOF'
+
+Welcome to the ngrok-operator development environment!
+
+Please make sure you have the following environment variables set:
+
+  NGROK_API_KEY      - Your ngrok API key
+  NGROK_AUTHTOKEN    - Your ngrok authtoken
+
+If you are using GitHub Codespaces, a kind cluster should
+already be running. You can verify this by running:
+
+  kind get clusters
+
+Common commands:
+  make build          - Build the operator
+  make test           - Run tests
+  make lint           - Run linters
+  make deploy         - Deploy to the kind cluster
+
+For more information, see the development documentation in
+
+  ./docs/developer-guide/README.md
+
+You can also run "devhelp" at any time to see this message again.
+EOF
+          '')
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
@@ -28,15 +67,20 @@
             kubectl
             kubernetes-helm
             kyverno-chainsaw
+            ngrok
             nixfmt-rfc-style
             tilt
             yq
-          ];
+          ] ++ scripts;
 
           CGO_ENABLED = "0";
           # GitHub Codespaces sets GOROOT in /etc/environment. However, we are managing
           # go via nix, so we need to unset it to avoid conflicts. See also: https://dave.cheney.net/2013/06/14/you-dont-need-to-set-goroot-really
           GOROOT = "";
+
+          shellHook = ''
+            devhelp
+          '';
         };
       }
     ));
