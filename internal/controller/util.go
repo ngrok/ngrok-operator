15d5fa748eb0ff74a3f19a5eb68bb4b27e597874
Jonathan Stacks <jonstacks@users.noreply.github.com>
2025-10-28 18:47:31 -0500

    chore: Add documentation for our controller utilities
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>

--- internal/controller/util.go
+++ internal/controller/util.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"maps"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -11,26 +12,36 @@ const (
 	FinalizerName = "k8s.ngrok.com/finalizer"
 )
 
+// IsUpsert returns true if the object is being created or updated.
+// i.e. the DeletionTimestamp is zero.
 func IsUpsert(o client.Object) bool {
 	return o.GetDeletionTimestamp().IsZero()
 }
 
+// IsDelete returns true if the object is being deleted.
+// i.e. the DeletionTimestamp is non-zero.
 func IsDelete(o client.Object) bool {
 	return !o.GetDeletionTimestamp().IsZero()
 }
 
+// HasFinalizer returns true if the object has our operator finalizer set.
 func HasFinalizer(o client.Object) bool {
 	return controllerutil.ContainsFinalizer(o, FinalizerName)
 }
 
+// AddFinalizer adds our operator finalizer to the object.
+// If the finalizer was not already present, it returns true.
 func AddFinalizer(o client.Object) bool {
 	return controllerutil.AddFinalizer(o, FinalizerName)
 }
 
+// RemoveFinalizer removes our operator finalizer from the object.
+// If the finalizer was present and removed, it returns true.
 func RemoveFinalizer(o client.Object) bool {
 	return controllerutil.RemoveFinalizer(o, FinalizerName)
 }
 
+// RegisterAndSyncFinalizer adds our finalizer to the object and updates it in the cluster if not already present.
 func RegisterAndSyncFinalizer(ctx context.Context, c client.Writer, o client.Object) error {
 	if !HasFinalizer(o) {
 		AddFinalizer(o)
@@ -39,11 +50,13 @@ func RegisterAndSyncFinalizer(ctx context.Context, c client.Writer, o client.Obj
 	return nil
 }
 
+// RemoveAndSyncFinalizer removes our finalizer from the object and updates it in the cluster.
 func RemoveAndSyncFinalizer(ctx context.Context, c client.Writer, o client.Object) error {
 	RemoveFinalizer(o)
 	return c.Update(ctx, o)
 }
 
+// AddAnnotations adds the given annotations to the object.
 func AddAnnotations(o client.Object, annotations map[string]string) {
 	if o == nil || annotations == nil {
 		return
@@ -54,9 +67,6 @@ func AddAnnotations(o client.Object, annotations map[string]string) {
 		existing = make(map[string]string)
 	}
 
-	for k, v := range annotations {
-		existing[k] = v
-	}
-
+	maps.Copy(existing, annotations)
 	o.SetAnnotations(existing)
 }
