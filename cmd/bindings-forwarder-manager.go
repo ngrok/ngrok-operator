8e7a76e6baea6efcf995d42481da3852cbde2645
Sabrina Ahmed <sabrina@ngrok.com>
2026-03-09 13:20:28 -0500

    Bug fixes for Pod Identity (#774)
    * Bug fixes
    
    * Apply suggestion from @sabrina-ngrok
    
    * Remove role permissions for pods

--- cmd/bindings-forwarder-manager.go
+++ cmd/bindings-forwarder-manager.go
@@ -32,10 +32,12 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -112,6 +114,9 @@ func runController(_ context.Context, opts bindingsForwarderManagerOpts) error {
 		Cache: cache.Options{
 			DefaultNamespaces: map[string]cache.Config{
 				opts.namespace: {},
+			},
+			ByObject: map[client.Object]cache.ByObject{
+				&corev1.Pod{}: {Namespaces: map[string]cache.Config{cache.AllNamespaces: {}}},
 			},
 		},
 		Metrics: server.Options{
